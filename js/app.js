ba7c3d4891aba288a9c7ae9be25d05363de00ff4
Inku <hermosillasotomayordiego@gmail.com>
2025-05-28 16:36:54 -0400

    pe

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let agregarDato=(event)=>{
+    event.preventDefault();
+
+    let tipo = document.getElementById("tipo").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let valor = document.getElementById("valor").value;
+
+
+if(descripcion!="" && valor !== ""){
+    console.log("descripcion: "+ descripcion);
+    console.log("valor4: "+ valor);
+}else{
+    alert("Debe completar todos los campos");
+}
+
+}
