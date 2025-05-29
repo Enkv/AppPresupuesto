4acd43414d15133adfa0fa4f6e5aedeea7e936f4
Inku <hermosillasotomayordiego@gmail.com>
2025-05-29 16:31:39 -0400

    aavances (recordar hacer el egreso)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+
+let ingresosHTML;
+let totalIngresos=0;
+
 let agregarDato=(event)=>{
-    event.preventDefault();
+    event.preventDefault(); //evita que la pagina se recargue al hacer click
 
     let tipo = document.getElementById("tipo").value;
     let descripcion = document.getElementById("descripcion").value;
@@ -8,9 +12,83 @@ let agregarDato=(event)=>{
 
 if(descripcion!="" && valor !== ""){
     console.log("descripcion: "+ descripcion);
-    console.log("valor4: "+ valor);
+    console.log("valor: "+ valor);
+    if(tipo==="ingreso"){
+        //hace algo
+        cargarIngresos(descripcion, Number(valor))
+
+    }else if(tipo==="egreso"){
+        //hace otra cosa
+        cargarEgreso(descripcion, Number(valor))
+    }
+    document.getElementById("descripcion").value="";
+    document.getElementById("valor").value="";
+
 }else{
     alert("Debe completar todos los campos");
 }
+console.log(tipo);
 
 }
+
+
+let cargarIngresos = (descripcion, valor) =>{
+    //ingresosHTML = ingresosHTML + crearIngresosHTML(descripcion, valor)  
+    ingresosHTML += crearIngresosHTML(descripcion, valor)
+    totalIngresos+=valor;
+    document.getElementById("ingresoTotal").textContent = formatearCLP(totalIngresos);
+    document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos);// modi
+    document.getElementById("lista-ingresos").innerHTML=ingresosHTML;
+
+}
+
+
+let crearIngresosHTML=(descripcion, valor)=>{
+    return `<div class="elemento limpiarEstilos">
+                <div class="elemento_descripcion">${descripcion}</div>
+                <div class="derecha limpiarEstilos">
+                    <div class="elemento_valor">${formatearCLP(valor)}</div>
+                    <div class="elemento_eliminar">
+                        <button class="elemento_eliminar--btn">
+                            <ion-icon name="close-circle-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+            </div>`;
+ }
+
+
+ let cargarEgresos = (descripcion, valor) =>{
+    //ingresosHTML = ingresosHTML + crearIngresosHTML(descripcion, valor)  
+    ingresosHTML -= crearEgresosHTML(descripcion, valor)
+    totalIngresos-=valor;
+    document.getElementById("ingresoTotal").textContent = formatearCLP(totalIngresos);
+    document.getElementById("presupuesto").textContent = formatearCLP(totalIngresos);// modi
+    document.getElementById("lista-ingresos").innerHTML=ingresosHTML;
+
+}
+
+
+let crearEgresosHTML=(descripcion, valor)=>{
+    return `<div class="elemento limpiarEstilos">
+                <div class="elemento_descripcion">${descripcion}</div>
+                <div class="derecha limpiarEstilos">
+                    <div class="elemento_valor">${formatearCLP(valor)}</div>
+                    <div class="elemento_eliminar">
+                        <button class="elemento_eliminar--btn">
+                            <ion-icon name="close-circle-outline"></ion-icon>
+                        </button>
+                    </div>
+                </div>
+            </div>`;
+ }
+
+
+ function formatearCLP(numero) {
+    return new Intl.NumberFormat('es-CL', {
+        style: 'currency',
+        currency: 'CLP',
+        minimumFractionDigits: 0
+    }).format(numero);
+}
+
